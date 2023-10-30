bded969abde6588b5f4e708c2f8e30ea8b615f03
Pradeep M Wedagedara <148761747+pradeepmwedagedara@users.noreply.github.com>
2023-10-30 12:51:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def data_calculate(sub_result, sub_credit): #Calculates total points for a subje
         Total_points_array.append(Total_points)
         Total_credit_array.append(sub_credit)
 
-
 def input_data(student_index_no, GPA): #input student_index_no and GPA to a dictionary
     data_dict[student_index_no] = GPA
     print("Data inserted to dictionary.")
